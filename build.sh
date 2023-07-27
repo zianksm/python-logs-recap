1b85209f4457e60d914388e95901ff3bf3e01882
zian <mynameiszian@gmail.com>
2023-07-27 13:30:15 +0800

    add build scripts

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm -rf app 
-mkdir app
-cp run.sh ./app
-cp rekap.py ./app
+rm -rf auto-rekap 
+mkdir auto-rekap
+cp run.sh ./auto-rekap
+cp rekap.py ./auto-rekap
 
-tar -zcvf app.tar.gz ./app
-rm -rf app 
+tar -zcvf auto-rekap.tar.gz auto-rekap
+rm -rf auto-rekap 
 
 
 
